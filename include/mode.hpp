67bdf074ab16426a4509cbc67b5b640ed5a33f08
Artemy <oas2870@gmail.com>
2021-05-12 22:13:21 +0700

    [fix] problem of choosing the mode of operation of the program

--- include/mode.hpp
+++ include/mode.hpp
@@ -1,18 +1,16 @@
 #pragma once
 
-#define __CV_TESTING__
+//#define __CV_TESTING__
 //#define __LINE_TESTING__
-//#define __FULL_PROGRAM__
+#define __FULL_PROGRAM__
 
 #if (defined(__CV_TESTING__) || defined(__FULL_PROGRAM__))
 #   define __CV_INCLUDE__
 #endif
 
-#if defined(__LINE_TESTING__)
-#   if defined(__CV_INCLUDE__)
-#       define __LINE_INCLUDE__     2
-#       define __FULL_REACTION__
-#   else
-#       define __LINE_INCLUDE__     1
-#   endif
+#if defined(__FULL_PROGRAM__)
+#	define __LINE_INCLUDE__     2
+#	define __FULL_REACTION__
+#elif defined(__LINE_TESTING__)
+#	define __LINE_INCLUDE__     1
 #endif
